correct units in macemuscript.py

mace/macemuscript.py
@@ -9,7 +9,7 @@ import numpy as np
 import time
 
 au_to_debye = 2.54174692380177980056
-
+au_to_ang = 0.529177
 
 ##################################################################################################
 
@@ -25,12 +25,11 @@ def setup_singlepoint(input_file, model_type='DipoleMACE', model_path=None):
     atoms = read(input_file)
     calc = MACECalculator(model_path, device='cpu', default_dtype='float64', model_type=model_type)
     start=time.time()
-    dipole = get_dipole(atoms, calc)
+    dipole = get_dipole(atoms, calc)/au_to_ang
     finish=time.time()
     totdipole = np.linalg.norm(dipole)
-    dipole = dipole 
-    print(f"Molecular dipole moment (x,y,z): {dipole[0]} {dipole[1]} {dipole[2]} \n")
-    print(f"Total dipole moment: {totdipole} \n")
+    print(f"Molecular dipole moment (x,y,z)/au: {dipole[0]} {dipole[1]} {dipole[2]} \n")
+    print(f"Total dipole moment au, D: {totdipole} {totdipole*au_to_debye} \n")
     print(f"time {finish-start:.3f} s")
     return atoms,calc
 
@@ -65,12 +64,12 @@ def calculate_numerical_hessian(atoms, calc, displace=1e-3):
             # Positive displacement
             atoms.positions[i][j] += dx
             #forces_plus = -atoms.get_forces()
-            dipole_plus = get_dipole(atoms, calc) / au_to_debye            
+            dipole_plus = get_dipole(atoms, calc) / au_to_ang            
 
             # Negative displacement
             atoms.positions[i][j] -= 2 * dx
             #forces_minus = -atoms.get_forces()
-            dipole_minus = get_dipole(atoms, calc) / au_to_debye
+            dipole_minus = get_dipole(atoms, calc) / au_to_ang
             
             # Reset the position
             atoms.positions[i][j] = original_positions[i][j]