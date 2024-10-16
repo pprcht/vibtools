62298ac587c2df1cd1608c01176a609ed1f12f6f
Philipp Pracht <research@philipp-pracht.de>
2024-10-16 14:56:07 +0200

    Make multiplot routines exportable

--- python/pyvibtools/__init__.py
+++ python/pyvibtools/__init__.py
@@ -4,15 +4,17 @@ from ._vibtools import __version__
 from .calculator import vibtoolsCalculator, matchscore
 from .readers import *
 from .printouts import *
+from .multiplot import *
 
 # Dynamically add all functions from the readers module to __all__
 import inspect
 from . import readers
 from . import printouts
+from . import multiplot
 
 __all__ = ["__version__", "vibtoolsCalculator", "matchscore"]
 
 # Add all functions from readers module to __all__
 __all__.extend(name for name, obj in inspect.getmembers(readers) if inspect.isfunction(obj))
 __all__.extend(name for name, obj in inspect.getmembers(printouts) if inspect.isfunction(obj))
-
+__all__.extend(name for name, obj in inspect.getmembers(multiplot) if inspect.isfunction(obj))
